58ef9c71d7d657d953c3758b93db0ffa26bc0283
Syntax Services <support@string.com.ng>
2026-09-12 10:28:17 +0000

    chore: remove expo-updates and url-polyfill to fix startup crash

--- mobile/index.js
+++ mobile/index.js
@@ -1,20 +1,6 @@
-import 'react-native-url-polyfill/auto';
 import { registerRootComponent } from 'expo';
-import { Alert } from 'react-native';
 import App from './App';
-
-// Catch any fatal JS errors during module load or outside React
-const defaultErrorHandler = ErrorUtils.getGlobalHandler();
-ErrorUtils.setGlobalHandler((error, isFatal) => {
-  if (isFatal) {
-    Alert.alert(
-      'Fatal JS Error',
-      `${error.name}: ${error.message}\n\nPlease take a screenshot of this and send it to support.`,
-      [{ text: 'OK' }]
-    );
-  }
-  defaultErrorHandler(error, isFatal);
-});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 registerRootComponent(App);
+
